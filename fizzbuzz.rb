e186b475397f31beefb4d4bbbf58082aae1c7d2a
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-29 16:21:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0
-    "buzz"
+    "Buzz"
   else int % 3 && 5 == 0
-    "fizzbuzz"
-    
+    "FizzBuzz"
+
   end
 end
